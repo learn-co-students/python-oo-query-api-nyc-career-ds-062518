c939d3393c2a9ba3a8b9dc5b5c25f11f3bdc050a
Jake <jakemacnaughton@gmail.com>
2018-06-12 18:06:51 -0400

    wrote first half instructions

--- app/setlist_data.py
+++ app/setlist_data.py
@@ -1,15 +1,17 @@
 import requests
 import json
 
-
+# Make your API request here
 token = 'a40dcb2e-cbe0-4b88-8b79-f43e451aa71a'
 headers = {'Accept': 'application/json', 'x-api-key': token}
+
 response = requests.get('https://api.setlist.fm/rest/1.0/search/setlists?artistName=The%20Beatles&p=1&year=1965', headers=headers)
 
 raw_data = json.loads(response.content)
 
 # Uncomment the python debugger to play our data in the terminal
 # import pdb; pdb.set_trace()
+
 
 # Data is a dictionary
 # check out data.keys()
