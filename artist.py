759fea9394fffe5b5762cefefb53a7c3b0a7e877
Adomkay <sh_adom@outlook.com>
2018-07-15 16:11:12 -0400

    Done.

--- artist.py
+++ artist.py
@@ -2,4 +2,27 @@ from show import Show
 
 # Write the Artist model here:
 class Artist:
-    pass
+    _all = []
+    def __init__(self, name):
+        Artist._all.append(self)
+        self._name = name
+
+    @classmethod
+    def all(cls):
+        return cls._all
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        self._name = name
+
+    def shows(self):
+        return [show.artist for show in Show.all() if show.artist == self]
+
+    @classmethod
+    def find_by_name(cls,name):
+        return [artist for artist in cls.all() if artist.name == name]
+        
--- etl.py
+++ etl.py
@@ -3,7 +3,14 @@ from artist import Artist
 from show import Show
 
 def find_or_create_artist(name):
-    pass
+    for artist in Artist.all():
+        if artist.name == name:
+            return artist
+    else:
+        return Artist(name)
 
 def make_show_objects(data):
-    pass
+    for i in range(0,len(data)):
+        Show(artist = find_or_create_artist(data[i]['artist']['name']), date =  data[i]['eventDate'], venue =  data[i]['venue']['name'], location = (data[i]['venue']['city']['name'] + ', ' + data[i]['venue']['city']['state']), setlist = [item['name'] for item in data[i]['sets']['set'][0]['song']])
+    return
+# import pdb; pdb.set_trace()
