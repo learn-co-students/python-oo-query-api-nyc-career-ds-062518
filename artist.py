ae7fecfdb8bbbee4e40753971a3a56e0eab68b91
Mark Rethana <mark.rethana@gmail.com>
2018-07-15 20:17:04 -0400

    Done.

--- artist.py
+++ artist.py
@@ -2,4 +2,27 @@ from show import Show
 
 # Write the Artist model here:
 class Artist:
-    pass
+    _all = []
+
+    def __init__(self, name):
+        self._name = name
+        Artist._all.append(self)
+
+    @classmethod
+    def all(cls):
+        return cls._all
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        self._name = name
+
+    def shows(self):
+        return [show for show in Show.all() if self == show._artist]
+
+    @classmethod
+    def find_by_name(cls, name):
+        return [artist for artist in cls.all() if artist._name == name]
--- setlist_data.py
+++ setlist_data.py
@@ -1,18 +1,33 @@
 import requests
 import json
-from config import API_KEY
+from config import *
 
 # Make your API request here
-
-
-
+headers = {'Accept': 'application/json', 'x-api-key': token}
+response = requests.get('https://api.setlist.fm/rest/1.0/search/setlists?artistName=The%20Beatles&p=1&year=1965', headers=headers)
+raw_data = json.loads(response.content)
 
 # Uncomment the python debugger to play our data in the terminal
 # import pdb; pdb.set_trace()
-
+#
+# for i in raw_data.keys():
+#     print(i)
+#     value = raw_data[i]
+#     print(type(value))
+#
+# type
+# <class 'str'>
+# itemsPerPage
+# <class 'int'>
+# page
+# <class 'int'>
+# total
+# <class 'int'>
+# setlist
+# <class 'list'>
 
 # Data is a dictionary
 # check out data.keys()
 
 # We want to clean our data to be a list of setlist dictionaries
-data = None
+data = [setlist for setlist in raw_data['setlist']]
