6a07710fbcdcf99abd33a9b33016eb3afe08916a
Sam Liebman <sammyliebman@gmail.com>
2018-07-16 10:39:03 -0400

    Done.

--- artist.py
+++ artist.py
@@ -1,5 +1,31 @@
 from show import Show
 
 # Write the Artist model here:
+
 class Artist:
-    pass
+    _all = []
+
+    def __init__(self, name):
+        self._name = name
+        Artist._all.append(self)
+
+    @classmethod
+    def all(cls):
+        return cls._all
+
+    def shows(self):
+        return [show for show in Show._all if show.artist.name == self.name]
+
+    @classmethod
+    def find_by_name(cls, name):
+        #return [artist for artist in cls._all if artist.name == name]
+        for artist in cls._all:
+            if artist.name == name:
+                return artist
+
+    @property
+    def name(self):
+        return self._name
+    @name.setter
+    def name(self, name):
+        self._name = name
